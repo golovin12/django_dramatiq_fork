dda0b066e19e7a53e6450d7bef85f15201ced409
Denis Golovin <golovin.denis@lenta.ru>
2026-02-15 16:26:18 +0500

    fix test case

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -5,19 +5,20 @@ from django_dramatiq.models import Task
 
 def test_tasks_excluded_actors(transactional_db, broker, worker):
     @dramatiq.actor
-    def not_ignored_actor_for_db():
-        pass
-
-    @dramatiq.actor
     def ignored_actor_for_db():
         pass
 
-    not_ignored_actor_for_db.send()
-    broker.join(not_ignored_actor_for_db.queue_name)
-    worker.join()
+    @dramatiq.actor
+    def not_ignored_actor_for_db():
+        pass
 
     ignored_actor_for_db.send()
     broker.join(ignored_actor_for_db.queue_name)
+    worker.join()
+    assert Task.tasks.count() == 0
+
+    not_ignored_actor_for_db.send()
+    broker.join(not_ignored_actor_for_db.queue_name)
     worker.join()
 
     assert Task.tasks.count() == 1
